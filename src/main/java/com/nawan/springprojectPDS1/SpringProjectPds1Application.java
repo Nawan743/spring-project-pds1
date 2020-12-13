6ae729608b4d01c0ecfcc83a42c448c02d5dd41e
Nawan743 <nawandx743@gmail.com>
2020-12-13 13:49:31 -0300

    Initial commit

---
+++ src/main/java/com/nawan/springprojectPDS1/SpringProjectPds1Application.java
@@ -0,0 +1,13 @@
+package com.nawan.springprojectPDS1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectPds1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectPds1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nawan/springprojectPDS1/SpringProjectPds1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nawan.springprojectPDS1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectPds1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
