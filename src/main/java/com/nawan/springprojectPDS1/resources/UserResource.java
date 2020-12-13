75206ae82e39c8a25abd675931d30d8b0c7974d2
Nawan743 <nawandx743@gmail.com>
2020-12-13 18:05:23 -0300

    User insert

--- src/main/java/com/nawan/springprojectPDS1/resources/UserResource.java
+++ src/main/java/com/nawan/springprojectPDS1/resources/UserResource.java
@@ -1,11 +1,17 @@
 package com.nawan.springprojectPDS1.resources;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nawan.springprojectPDS1.entities.User;
 import com.nawan.springprojectPDS1.services.UserService;
@@ -16,11 +22,11 @@ public class UserResource {
 	
 	@Autowired
 	private UserService service;
-	
+
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {	
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
@@ -28,4 +34,13 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
 }
