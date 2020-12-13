bca641fcc59d1c9f14326a206b81d072435cfe5e
Nawan743 <nawandx743@gmail.com>
2020-12-13 14:54:36 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/nawan/springprojectPDS1/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.nawan.springprojectPDS1.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nawan.springprojectPDS1.entities.Order;
+import com.nawan.springprojectPDS1.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
