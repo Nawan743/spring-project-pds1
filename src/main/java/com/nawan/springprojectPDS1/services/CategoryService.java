40d05767e72181caf6a02e49f409cbbb9ffa1365
Nawan743 <nawandx743@gmail.com>
2020-12-15 16:30:46 -0300

    Challenge: CategoryDTO, ProductDTO

--- src/main/java/com/nawan/springprojectPDS1/services/CategoryService.java
+++ src/main/java/com/nawan/springprojectPDS1/services/CategoryService.java
@@ -2,26 +2,68 @@ package com.nawan.springprojectPDS1.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.nawan.springprojectPDS1.dto.CategoryDTO;
 import com.nawan.springprojectPDS1.entities.Category;
 import com.nawan.springprojectPDS1.repositories.CategoryRepository;
+import com.nawan.springprojectPDS1.services.exceptions.DatabaseException;
+import com.nawan.springprojectPDS1.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
-	public List<Category> findAll() {
-		return repository.findAll();
-	}
-	
-	public Category findById(Long id) {
-		Optional<Category> obj = repository.findById(id);
-		return obj.get();
+
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(e -> new CategoryDTO(e)).collect(Collectors.toList());
 	}
 
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
+		return new CategoryDTO(entity);
+	}
+
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = dto.toEntity();
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id);
+			updateData(entity, dto);
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Category entity, CategoryDTO dto) {
+		entity.setName(dto.getName());
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
 }
