ed978c5154b46cc6d98418fb59a05b36708e4e6f
Nawan743 <nawandx743@gmail.com>
2021-01-13 12:46:06 -0300

    OrderDTO

--- src/main/java/com/nawan/springprojectPDS1/resources/OrderResource.java
+++ src/main/java/com/nawan/springprojectPDS1/resources/OrderResource.java
@@ -9,26 +9,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nawan.springprojectPDS1.entities.Order;
+import com.nawan.springprojectPDS1.dto.OrderDTO;
 import com.nawan.springprojectPDS1.services.OrderService;
 
 @RestController
 @RequestMapping(value = "/orders")
 public class OrderResource {
-	
+
 	@Autowired
 	private OrderService service;
 
 	@GetMapping
-	public ResponseEntity<List<Order>> findAll() {	
-		List<Order> list = service.findAll();
+	public ResponseEntity<List<OrderDTO>> findAll() {
+		List<OrderDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Order> findById(@PathVariable Long id) {
-		Order obj = service.findById(id);
+	public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
+		OrderDTO obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 }
--- src/main/java/com/nawan/springprojectPDS1/services/OrderService.java
+++ src/main/java/com/nawan/springprojectPDS1/services/OrderService.java
@@ -2,26 +2,31 @@ package com.nawan.springprojectPDS1.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.nawan.springprojectPDS1.dto.OrderDTO;
 import com.nawan.springprojectPDS1.entities.Order;
 import com.nawan.springprojectPDS1.repositories.OrderRepository;
+import com.nawan.springprojectPDS1.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class OrderService {
-	
+
 	@Autowired
 	private OrderRepository repository;
-	
-	public List<Order> findAll() {
-		return repository.findAll();
+
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findAll();
+		return list.stream().map(e -> new OrderDTO(e)).collect(Collectors.toList());
 	}
-	
-	public Order findById(Long id) {
+
+	public OrderDTO findById(Long id) {
 		Optional<Order> obj = repository.findById(id);
-		return obj.get();
+		Order entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
+		return new OrderDTO(entity);
 	}
 
 }
